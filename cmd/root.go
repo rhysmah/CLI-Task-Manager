545cdba7f92b732d5dbc75418f6e45c04cda5c90
Rhys Mahannah <rhys.mahannah@gmail.com>
2024-06-24 15:36:40 -0700

    feat: add flags to `list` command

--- cmd/root.go
+++ cmd/root.go
@@ -10,13 +10,15 @@ import (
 	"github.com/spf13/cobra"
 )
 
-var tasks = make(map[string]bool)
+var (
+	showCompleted   bool
+	showIncompleted bool
+	tasks           = make(map[string]bool)
+)
 
 /*
 `rootCmd` is the root command for the CLI application
-This is what's called when the CLI application is run
-for the first time -- in this case, by `CLI-Task-Manager`,
-the name of the module.
+This is what's called when the CLI application is run.
 */
 var rootCmd = &cobra.Command{
 	Use:   "task",
@@ -41,19 +43,26 @@ var listCmd = &cobra.Command{
 	Use:   "list",
 	Short: "List all tasks in your task list.",
 	Run: func(cmd *cobra.Command, args []string) {
+
+		// Get flag values
+		showCompleted, _ := cmd.Flags().GetBool("completed")
+		showUncompleted, _ := cmd.Flags().GetBool("uncompleted")
+
 		if len(tasks) == 0 {
 			fmt.Println("You currently have no tasks in your list!")
+			return
+		}
 
-		} else {
-			fmt.Println("####################")
-			fmt.Println("#### Your Tasks ####")
-			fmt.Println("####################")
-			for task, isComplete := range tasks {
-				if isComplete {
-					fmt.Println("[✓]", task)
-				} else {
-					fmt.Println("[ ]", task)
-				}
+		filteredTasks := filterTasks(tasks, showCompleted, showUncompleted)
+
+		fmt.Println("####################")
+		fmt.Println("#    Your Tasks    #")
+		fmt.Println("####################")
+		for task, isComplete := range filteredTasks {
+			if isComplete {
+				fmt.Println("[✓]", task)
+			} else {
+				fmt.Println("[ ]", task)
 			}
 		}
 	},
@@ -99,4 +108,23 @@ func init() {
 		pass to the command when they run the CLI application.
 	*/
 	addCmd.Flags().StringP("task", "t", "", "The task to add to your task list.")
+
+	listCmd.Flags().BoolVarP(&showCompleted, "completed", "c", false, "Show completed tasks.")
+	listCmd.Flags().BoolVarP(&showIncompleted, "uncompleted", "u", false, "Show uncompleted tasks.")
+}
+
+func filterTasks(tasks map[string]bool, showCompleted, showIncompleted bool) map[string]bool {
+	if !showCompleted && !showIncompleted {
+		return tasks
+	}
+
+	filteredTasks := make(map[string]bool)
+	for task, isComplete := range tasks {
+		if showCompleted && isComplete {
+			filteredTasks[task] = isComplete
+		} else if showIncompleted && !isComplete {
+			filteredTasks[task] = isComplete
+		}
+	}
+	return filteredTasks
 }
