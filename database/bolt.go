273232f84c5ab810f0caa495c428cfc3f1f304cd
Rhys Mahannah <rhys.mahannah@gmail.com>
2024-06-27 10:10:29 -0700

    refactor: add helper function, update db operations
    created helper function to handle database opening / closing; updated `add` and `list` operations

--- database/bolt.go
+++ database/bolt.go
@@ -2,7 +2,6 @@ package database
 
 import (
 	"fmt"
-	"log"
 	"strconv"
 
 	"github.com/boltdb/bolt"
@@ -12,50 +11,9 @@ const dbFileName = "tasks.db"
 const bucketName = "Tasks"
 const readWriteCode = 0600
 
-func ListTasks() (map[string]bool, error) {
+func withDatabase(operation func(db *bolt.DB) error) error {
 
-	fmt.Println("Opening database...")
-	db, err := bolt.Open(dbFileName, readWriteCode, nil)
-	if err != nil {
-		return nil, fmt.Errorf("error opening database: %s", err)
-	}
-	defer db.Close()
-
-	tasks := make(map[string]bool)
-
-	// Begin a read-only transaction
-	fmt.Println("Reading tasks in database...")
-	err = db.View(func(transaction *bolt.Tx) error {
-
-		// Retrieve bucket
-		bucket := transaction.Bucket([]byte(bucketName))
-		if bucket == nil {
-			return fmt.Errorf("%s not found: ", bucketName)
-		}
-
-		return bucket.ForEach(func(k, v []byte) error {
-
-			taskAsString := string(k)
-			valueAsString := string(v)
-			valueStringAsBool, err := strconv.ParseBool(valueAsString)
-			if err != nil {
-				return fmt.Errorf("error retrieving task value: %s", err)
-			}
-			tasks[taskAsString] = valueStringAsBool
-			return nil
-		})
-	})
-
-	if err != nil {
-		return nil, err
-	}
-	fmt.Println("Tasks successfully read from database!")
-	return tasks, err
-}
-
-func WriteTask(task string) error {
-
-	fmt.Println("Opening database...")
+	fmt.Println("Accessing database...")
 	db, err := bolt.Open(dbFileName, readWriteCode, nil)
 	if err != nil {
 		return fmt.Errorf("error opening database: %s", err)
@@ -63,34 +21,77 @@ func WriteTask(task string) error {
 
 	defer db.Close()
 
-	// Begin a read-write transaction
-	fmt.Println("Beginning to write task to database...")
-	err = db.Update(func(transaction *bolt.Tx) error {
-
-		// Creates bucket if it doesn't exist; a bucket is a collection
-		// of key-value pairs -- tasks and their status (complete or incomplete)
-		bucket, err := transaction.CreateBucketIfNotExists([]byte(bucketName))
-		if err != nil {
-			return fmt.Errorf("error creating bucket: %s", err)
-		}
-
-		// Default value for all task keys is "false"
-		// `bucket` requires the key and value to both be bytes
-		stringVal := strconv.FormatBool(false)
-		falseByteSlice := []byte(stringVal)
-
-		err = bucket.Put([]byte(task), falseByteSlice)
-		if err != nil {
-			return fmt.Errorf("error writing task to database: %s", err)
-		}
-		return nil
-	})
-
+	err = operation(db)
 	if err != nil {
-		log.Printf("Error writing task '%s' to database: %s\n", task, err)
 		return err
 	}
 
-	fmt.Printf("Task '%s' written to database successfully!\n", task)
-	return nil
+	return nil // Operations successful
+}
+
+func ListTasks() (map[string]bool, error) {
+
+	tasksFromDb := make(map[string]bool)
+
+	fmt.Println("Reading tasks in database...")
+	err := withDatabase(func(db *bolt.DB) error {
+
+		return db.View(func(t *bolt.Tx) error {
+
+			// Attempt to find bucket containing tasks
+			bucket := t.Bucket([]byte(bucketName))
+			if bucket == nil {
+				return fmt.Errorf("bucket %s not found: ", bucketName)
+			}
+
+			// Bucket found; iterate through tasks; write to map
+			return bucket.ForEach(func(k, v []byte) error {
+				taskAsString := string(k)
+
+				valueAsBool, err := strconv.ParseBool(string(v))
+				if err != nil {
+					return fmt.Errorf("error retrieving tasks %s", taskAsString)
+				}
+
+				tasksFromDb[taskAsString] = valueAsBool
+				return nil // operation successful
+			})
+		})
+	})
+
+	// Error performing operation
+	if err != nil {
+		return nil, err
+	}
+
+	fmt.Println("Successfully read tasks!")
+	return tasksFromDb, nil
+}
+
+func WriteTask(task string) error {
+
+	return withDatabase(func(db *bolt.DB) error {
+		err := db.Update(func(transaction *bolt.Tx) error {
+
+			bucket, err := transaction.CreateBucketIfNotExists([]byte(bucketName))
+			if err != nil {
+				return fmt.Errorf("error creating bucket: %s", err)
+			}
+
+			// Default value for all tasks is Boolean false
+			falseAsByteSlice := []byte(strconv.FormatBool(false))
+			err = bucket.Put([]byte(task), falseAsByteSlice)
+			if err != nil {
+				return fmt.Errorf("error writing task to database: %s", err)
+			}
+			return nil // Task successfull written to database
+		})
+
+		if err != nil {
+			return err
+		}
+
+		fmt.Printf("Task '%s' successfully written to database!\n", task)
+		return nil
+	})
 }
